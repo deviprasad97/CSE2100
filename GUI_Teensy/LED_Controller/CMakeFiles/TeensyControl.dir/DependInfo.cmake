77c3692a467e0f297653232fd40d90d1c7085ada
Devi Prasad Triathy <deviprasadtriathy@Devis-MacBook-Air.local>
2017-12-07 09:58:51 -0600

    CSE2100

---
+++ GUI_Teensy/LED_Controller/CMakeFiles/TeensyControl.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/deviprasadtriathy/Desktop/cse2100william/GUI_Teensy/LED_Controller/src/global.cpp" "/Users/deviprasadtriathy/Desktop/cse2100william/GUI_Teensy/LED_Controller/CMakeFiles/TeensyControl.dir/src/global.cpp.o"
+  "/Users/deviprasadtriathy/Desktop/cse2100william/GUI_Teensy/LED_Controller/src/main.cpp" "/Users/deviprasadtriathy/Desktop/cse2100william/GUI_Teensy/LED_Controller/CMakeFiles/TeensyControl.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "/usr/local/Cellar/gtk+3/3.22.26/include/gtk-3.0"
+  "/usr/local/Cellar/glib/2.54.2/include/gio-unix-2.0"
+  "/usr/local/Cellar/cairo/1.14.10/include/cairo"
+  "/usr/local/Cellar/libepoxy/1.4.3/include"
+  "/usr/local/Cellar/pango/1.40.14/include/pango-1.0"
+  "/usr/local/Cellar/harfbuzz/1.7.2_1/include/harfbuzz"
+  "/usr/local/Cellar/graphite2/1.3.10/include"
+  "/usr/local/Cellar/atk/2.26.1/include/atk-1.0"
+  "/usr/local/Cellar/pixman/0.34.0_1/include/pixman-1"
+  "/usr/local/Cellar/fontconfig/2.12.6/include"
+  "/usr/local/opt/freetype/include/freetype2"
+  "/usr/local/Cellar/libpng/1.6.34/include/libpng16"
+  "/usr/local/Cellar/gdk-pixbuf/2.36.11/include/gdk-pixbuf-2.0"
+  "/usr/local/Cellar/glib/2.54.2/include/glib-2.0"
+  "/usr/local/Cellar/glib/2.54.2/lib/glib-2.0/include"
+  "/usr/local/opt/gettext/include"
+  "/usr/local/Cellar/pcre/8.41/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ GUI_Teensy/LED_Controller/src/CMakeFiles/TeensyControl.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TeensyControl.dir/serialreadthread.cpp.o"
+  "CMakeFiles/TeensyControl.dir/main.cpp.o"
+  "CMakeFiles/TeensyControl.dir/global.cpp.o"
+  "TeensyControl.pdb"
+  "TeensyControl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TeensyControl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
